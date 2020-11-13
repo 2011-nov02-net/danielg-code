ec6129ffdd22a0102ac4ad75b530adbe664c7752
dannydjg16 <dannydjg16@gmail.com>
2020-11-13 08:54:14 -0500

    dbcontext scaffold

---
+++ EfDbFirstDemo/EfDbFirstDemo.DataModel/EmpDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EfDbFirstDemo.DataModel
+{
+    public partial class EmpDetail
+    {
+        public string Id { get; set; }
+        public string EmployeeId { get; set; }
+        public int Salary { get; set; }
+        public string AddressOne { get; set; }
+        public string AddressTwo { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+
+        public virtual Employee1 Employee { get; set; }
+    }
+}
